62e91ee3c59d1d16a4fa24a6413394bbeb898e5e
Santiago Cerutti <44842654+santiagocerutti@users.noreply.github.com>
2024-05-19 03:04:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import os
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Municipalities_with_topo.csv'))
 
 # Lista de variables
-#vars = df.columns[5:]
+vars = df.columns[5:]
 #vars
-table = df.groupby(by="CVE_ENT")['tri1k_mean'].mean().reset_index()
+table = df.groupby(by="CVE_ENT")[vars].mean().reset_index()
 
 #if st.checkbox('Show dataframe'):
 #df.columns
