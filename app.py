be667ab7c98ec9adb9e7a9bf2f939a50c0ee9572
Santiago Cerutti <44842654+santiagocerutti@users.noreply.github.com>
2024-05-19 03:06:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'Municipalities_with_topo.csv'))
 
 # Lista de variables
-vars = df.columns[5:]
-#vars
+vars = [tri1k_mean	,tri1k_stde	,tri1k_max	,vrm1k_mean	,vrm1k_stde	,vrm1k_max	,tpi1k_mean	,tpi1k_stde	,tpi1k_max	,slp1k_mean	,slp1k_stde	,slp1k_max	,rou1k_mean	,rou1k_stde	,rou1k_max	,ele1k_mean	,ele1k_stde	,ele1k_max]
+
 table = df.groupby(by="CVE_ENT")[vars].mean().reset_index()
 
 #if st.checkbox('Show dataframe'):
